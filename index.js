24cb227aa97ff3cf88e281b60fb41f0f96d40a9a
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-03-31 03:54:46 -0400

    My first chat application

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket  = require('socket.io');
+
+// App setup
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => console.log(`Hey the server's running on port: ${PORT}`));
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+let userCount = 0;
+
+io.on('connection', (socket) => {
+  userCount++;
+
+  // Cancels the "is typing" signal if the field is empty
+  socket.on('cancel-typing', () => socket.broadcast.emit('cancel-typing'));
+
+  // Shows "is typing" for when a user is typing a message
+  socket.on('typing', (data) => {
+      socket.broadcast.emit('typing', data);
+  });
+
+  // When the submit button is pressed the message is sent to all sockets
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,60 @@
+// Made connection
+const socket = io.connect('http://localhost:5000');
+
+let message = document.querySelector('#message');
+handle = document.querySelector('#handle');
+btn = document.querySelector('#send');
+output = document.querySelector('#output');
+feedback = document.querySelector('#feedback');
+
+// Emit events
+btn.addEventListener('click', () => {
+	if (message.value.length === 0) {
+		return;
+	} else {
+		socket.emit('chat', {
+			message: message.value,
+			handle: handle.value,
+      time: moment().add(3, 'days').calendar()
+		});
+		message.value = '';
+		socket.emit('cancel-typing');
+	}
+});
+
+message.addEventListener('keyup', e => {
+	let firstLetter = message.value.charCodeAt(0);
+	if (message.value.length === 0) {
+		socket.emit('cancel-typing');
+	} else if (e.keyCode === 13) {
+		if (firstLetter === 10) {
+			console.log('Maybe you should enter a message dude?');
+		} else {
+			socket.emit('chat', {
+				message: message.value,
+				handle: handle.value,
+        time: moment().add(3, 'days').calendar()
+			});
+		}
+    socket.emit('cancel-typing');
+    message.value = '';
+
+	} else {
+		socket.emit('typing', handle.value);
+	}
+});
+
+// Listener events
+
+socket.on('chat', data => {
+	output.innerHTML += `
+  <div>
+  <span>${data.time}</span>
+  <p><strong>${data.handle}:</strong>${data.message}</p></div>`;
+});
+
+socket.on('typing', data => {
+	feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
+});
+
+socket.on('cancel-typing', () => (feedback.innerHTML = ''));
